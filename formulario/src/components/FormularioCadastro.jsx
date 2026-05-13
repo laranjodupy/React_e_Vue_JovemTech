2d3adb319a9998b442093281f52884e7117ec1d9
laranjodupy <vianalimadaniel3@gmail.com>
2026-05-13 11:31:16 -0300

    adicionando condições para controle de formularios com funções de estade

--- formulario/src/components/FormularioCadastro.jsx
+++ formulario/src/components/FormularioCadastro.jsx
@@ -3,15 +3,31 @@ import BotaoEnviar from './BotaoEnviar.jsx'
 import { useState } from "react"
 
 function FormularioCadastro() {
-    const [nome, setNome] = useState('')
-    const [email, setEmail] = useState('')
-    const [telefone, setTelefone] = useState('')
+    const [nome, setNome] = useState('');
+    const [email, setEmail] = useState('');
+    const [telefone, setTelefone] = useState('');
+    const [erro, setErro] = useState('');
+    const [sucesso, setSucesso] = useState(false);
     const handleSubmit = (e) => {
-        e.preventDefault() // Trava o carregamento da página
+        e.preventDefault()
+
+        if (nome.trim('') === '') {
+            setErro('O campo nome está vazio. Preencha')
+            return 
+        }
+        if (telefone.trim().length() !== 11) {
+            setErro('O campo telefone deve conter 11 dígitos. Preencha corretamente')
+            return
+        }
+
+        setSucesso(true);
+        console.log({ nome, email, telefone })// Ele segura o comportamento padrão do formulário, que é recarregar a página, e permite que a gente faça o que quiser com os dados do formulário. Sem isso, a página recarregaria e perderíamos os dados que o usuário digitou.
     }
 
     return(
         <form onSubmit={handleSubmit}>
+            {erro && <p style={{color: 'red'}}>{erro}</p>}
+            {sucesso && <p style={{color: 'green'}}>Cadastrado com Sucesso</p>}
             <InputField 
                 label={"Nome: "} 
                 type={"text"} 
