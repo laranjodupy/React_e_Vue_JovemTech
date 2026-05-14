499d37945259ad3f75f5214290fbaf5ed7da962a
laranjodupy <vianalimadaniel3@gmail.com>
2026-05-14 10:34:34 -0300

    ajeitano: refatorando constantes para utilizar objetos e manipulando dados com eles (a partir de arrow functions anonimas)

--- formulario/src/components/FormularioCadastro.jsx
+++ formulario/src/components/FormularioCadastro.jsx
@@ -3,54 +3,55 @@ import BotaoEnviar from './BotaoEnviar.jsx'
 import { useState } from "react"
 
 function FormularioCadastro() {
-    const [nome, setNome] = useState('');
-    const [email, setEmail] = useState('');
-    const [telefone, setTelefone] = useState('');
-    const [erro, setErro] = useState('');
-    const [sucesso, setSucesso] = useState(false);
+   //const [erro, setErro] = useState('');
+    //const [sucesso, setSucesso] = useState(false);
+    const [validacao, setValidacao] = useState({erro: '', sucesso: false})
+    const [user, setUser] = useState({nome: '', email: '', telefone: ''})
     const handleSubmit = (e) => {
         e.preventDefault()
 
-        if (nome.trim('') === '') {
-            setErro('O campo nome está vazio. Preencha')
+        if (user.nome.trim() === '') {
+            setValidacao((dados) => ({...dados, erro: 'Nome vazio, preencha fdp'}))
+            console.log()
             return 
         }
-        if (telefone.trim().length() !== 11) {
-            setErro('O campo telefone deve conter 11 dígitos. Preencha corretamente')
+        if (user.telefone.trim().length !== 11) {
+            setValidacao((dados) => ({...dados, erro: 'Seu número não possui 11 caracteres '}))
             return
         }
 
-        setSucesso(true);
-        console.log({ nome, email, telefone })// Ele segura o comportamento padrão do formulário, que é recarregar a página, e permite que a gente faça o que quiser com os dados do formulário. Sem isso, a página recarregaria e perderíamos os dados que o usuário digitou.
+        setValidacao.sucesso((dados) => ({...dados, sucesso: true}));
+        console.log(user)// Ele segura o comportamento padrão do formulário, que é recarregar a página, e permite que a gente faça o que quiser com os dados do formulário. Sem isso, a página recarregaria e perderíamos os dados que o usuário digitou.
     }
 
     return(
         <form onSubmit={handleSubmit}>
-            {erro && <p style={{color: 'red'}}>{erro}</p>}
-            {sucesso && <p style={{color: 'green'}}>Cadastrado com Sucesso</p>}
+            {validacao.erro && <p style={{color: 'red'}}>{validacao.erro}</p>}
+            {validacao.sucesso && <p style={{color: 'green'}}>Cadastrado com Sucesso</p>}
             <InputField 
                 label={"Nome: "} 
                 type={"text"} 
                 name={"nome"} 
                 placeholder={"Digite seu nome"}
-                value={nome}
-                onChange={(e) => setNome(e.target.value)}
+                value={user.nome}
+                onChange={(e) => setUser((dados) => ({...dados, nome: e.target.value, telefone: user.telefone, email: user.email}))}
                 />
             <InputField
                 label={"Email: "} 
                 type={"email"} 
                 name={"email"} 
                 placeholder={"email@empresa.com"}
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
+                value={user.email}
+                onChange={(e) => setUser((dados) => ({...dados, nome: user.nome, telefone: user.telefone, email: e.target.value}))}
                 />
             <InputField 
                 label={"Telefone: "} 
                 type={"number"} 
                 name={"numero"} 
                 placeholder={"Digite seu número: "}
-                value={telefone}
-                onChange={(e) => setTelefone(e.target.value)}
+                value={user.telefone}
+                onChange={(e) => setUser((dados) => ({...dados, nome: user.nome, telefone: e.target.value, email: user.email}))}
+
                 />
             <InputField 
                 label={"Nascimento: "} 
@@ -76,8 +77,9 @@ function FormularioCadastro() {
                 texto={"Cadastrar"}/>
             <div>
             <p>
-                Nome do usuário:  {nome}
+                Nome do usuário:  {user.nome}
             </p>
+
         </div>
         </form>
     )   
