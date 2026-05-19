03773cfd84da4d35a4b2fc9f0605f9dd615ddbed
laranjodupy <vianalimadaniel3@gmail.com>
2026-05-19 11:24:22 -0300

    feat: corrigindo duplicatas; mas falta realizar o 3, já tem a função primerumaiusculu, basta usar ela na correção de duplicatas

--- servidor/index.js
+++ servidor/index.js
@@ -3,8 +3,11 @@ const express = require('express');
 const bilola = express(); //é o servidor
 bilola.use(cors())
 bilola.use(express.json()); //middleware para interpretar o corpo da requisição como json
+const primeromausculu = (nome) => {
+    return name.toLowerCase().split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).join(' ')) //essa função vai: deixar o nome em lower -> separar os espaços por "," -> criar um map que tem uma arrow funtion que trata cada palavra separando as primeiras letras com o indice charat -> substitui as ',' por ' ', com o join. #Funcionalidade = fazer validação de duplicatas com letras maiusculas e minusculas
+}
 
-
+//homens peludos
 
 const PORT = 3000;
 
@@ -23,26 +26,39 @@ bilola.get('/   ', (req, res) => {
         mensagem: 'ta de boa ai, funfando',
         dados: dados
     })
-    registros.push(dados)
 }) //comando para saber se o servidor esta funfando tranquilo a partir do get (requisição). Se sim, ele vai dar esse post la.
 
 //rota post para criar um novo registro
 bilola.post('/registros', (req, res) => {
     const dados = req.body // pega o corpo da requisição
     if (!dados.nome){
-        req.status(400).json({
+       return res.status(400).json({
             erro:"Campo de nome é Obrigatório!"
         })
     }
-    console.log(` Dados da requisição (o que há no corpo que o front enviou):
-        ${dados}`) // mostra os dados da requisição
-    registros.push(dados) // simulando salvar dados no banco
 
-    res.status(201).json( {
+    //desafio 1 - evitando duplicatas -- desafio 1 - evitando duplicatas -- desafio 1 - evitando duplicatas -- desafio 1 - evitando duplicatas
+    const nomedoMermo = registros.find(r=> r.email === dados.email)
+    const telefonedoMermo = registros.find(r=> r.telefone === dados.telefone)
+    if (nomedoMermo) {
+        return res.status(409).json({
+            erro: "Nome engual ai"
+        }) 
+    }
+    if (telefonedoMermo) {
+        return res.status(409).json({
+            erro: 'Já existe essa merda'
+        })
+    }
+
+   registros.push(dados) //adiciona os dados ao array de registros
+
+    return res.status(201).json( {
         sucesso: true,
         mensagem: "Criou a bosta do registro ai man",
         dados: dados
     })// comando que vê o status de algum numero que indica a resposta do servidor, por exemplo: o 201 significa que houve um post com sucesso, ele se chama 'Created'.
+
 })
 
 bilola.listen(PORT, () => {
