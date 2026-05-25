3d2db9aa39f84bcecf0e64beb9dab2e3f7dc527c
laranjodupy <vianalimadaniel3@gmail.com>
2026-05-25 11:41:48 -0300

    feat: adicionando as funções e usando elas para que o trabalho de adicionar as coisas no servidor seja trabalho apenas da api, sem se preocupar com isso

--- formulario/src/components/FormularioCadastro.jsx
+++ formulario/src/components/FormularioCadastro.jsx
@@ -5,7 +5,8 @@ import {useRegistros} from './hooks/useRegistros.js'
 import { use } from "react"
 
 function FormularioCadastro() {
-    const [validacao, setValidacao] = useState({ erro: '', sucesso: false })
+    const [erroFormulario, setErroFormulario] = useState ('') // criando um hook para evitar conflito com o erro do api
+    const [sucessoFormulario, setSucessoFormulario] = useState(false)
     const [user, setUser] = useState({ nome: '', email: '', telefone: '' })
     const [coisaslegais, setCoisasLegais] = useState([]) //busca os registros, deixei coisas legais porque são coisas legais
     const [atualizar, setAtualizar] = useState(false)
@@ -19,12 +20,24 @@ function FormularioCadastro() {
     const handleSubmit = async (e) => {
         e.preventDefault()
 
+        try{
+            if (editandoi !== null) {
+                await atualizar(editandoi, {nome,email,telefone})
+            } else {
+                await criar({nome,email,telefone})
+            }
+            setErroFormulario('') //limpa o erro
+        } catch (e){
+            setErroFormulario(e.message) //mostra o erro que o hook jogou
+            
+        }
+
         if (user.nome.trim() === "") {
-            return setValidacao({erro: "Tem q ter nome filho", sucesso: false})
+            return setErroFormulario('Põe o nome ai carai')
         }
 
         if (user.telefone.length !== 11) {
-            return setValidacao({erro: "Compre um telefone pra colocar o numero", sucesso: false})
+            return setErroFormulario('compre um telefone diferente ai')
         }
 
         // o setpodeusar e o settimeout são do desafio para fazer com que o botao fique desabilitado apos ser clicado
@@ -32,85 +45,6 @@ function FormularioCadastro() {
     setTimeout(() => {
         setPodeUsar({ pode: true, carregano: 'EnvíSamerda' })
         }, 3000) }
-
-
-    //     try {
-    //         const uerriele = editandoi !== null ? 'http://localhost:3000/registros${editandoi}' : 'http://3000/registros'
-    //         const method = editandoi !== null ? 'PUT': 'POST'
-    //         const resposta = await fetch(uerriele, {
-    //             method,
-    //             headers: {'Content-Type': 'application/json'},
-    //             body: JSON.stingfy(user)
-    //         })
-
-
-    //         const resultado = await resposta.json()
-    //         console.log(resposta)
-    //         const statusCode = resposta.status; //const que pega o codigo que retorna do servidor e armazena
-
-    //         if (statusCode == 409) {
-    //             setValidacao({erro: 'Deu erro de conflito ai' }) //como o statusCode é number, com ele é possivel fazer uma condição em cima do codigo para tratá-lo. Nesse caso, o setValidação é alterado na chave erro
-    //         }
-
-    //         if (statusCode == 201) {
-    //             setAtualizar(!atualizar)
-    //         }
-
-    //         if (statusCode == 200) {
-    //             setAtualizar(!atualizar)
-    //         }
-
-    //         if (statusCode == 201) {
-    //             setAtualizar(!atualizar)
-    //         }
-
-    //         if (statusCode == 200) {
-    //             setAtualizar(!atualizar)
-    //         }
-
-    //         setValidacao({erro: '', sucesso: true})
-    //         setUser({nome: '', email: '', telefone: ''})
-
-    //         setEditandoI(null) // essencial para voltar para omodo de criação
-    //         buscarCoisasLegais()
-
-
-    //     } catch (error) {
-    //         //desafio 
-    //         console.log('Táerradomizera', error) // da um catch no erro
-            
-            
-
-    //     }
-        
-    // } 
-    // const handleDeletar = async (index) => {
-    //     console.log(index)
-    //     const confirmou = window.confirm('Deseja remote este registro?') // forma nativa do navegador perguntar algo antes de fazer tlg
-    //     if (!confirmou) return
-
-    //     try{
-    //         const resposta = await fetch('http//:3000/registros/${index}', {method: 'DELETE'})
-    //         if (!resposta.ok) {
-    //             const dados = await resposta.json()
-    //             setErro(dados.erro)
-    //             return
-    //         }
-    //         buscarCoisasLegais() // atualiza na tela
-    //     } catch {
-    //         setValidacao('Erro ao remover. Verifique o servidor')
-    //     }
-    // }
-
-    // const handleEditar = (index) => {
-    //     const registro = registros[index]
-
-    //     //preenchendo os campos com os dados existentes
-    //     setUser({nome: registro.nome, email: registro.nome, telefone: registro.telefone})
-    //     setEditandoI(index)
-
-    //     nomeRef.current.focus()
-    // }
     
     /* -- return:
     - a validacao.erro abaixo pega o valor alterado no setValidacao(que altera ao mesmo tempo que o é trocado, mas não é um hook
@@ -118,8 +52,8 @@ function FormularioCadastro() {
     - observação, no onChange de nome, percebe-se que é possível adicionar varias funções numa arrow function. Nesse caso, o setUser e o setValidação trabalham ao mesmo tempo nessa função.*/
     return (
         <form onSubmit={handleSubmit}>
-            {validacao.erro && <p style={{ color: 'red' }}>{validacao.erro}</p>}
-            {validacao.sucesso && <p style={{ color: 'green' }}>Cadastrado com Sucesso</p>}
+            {erroFormulario && <p style={{ color: 'red' }}>{erroFormulario}</p>}
+            {sucessoFormulario && <p style={{ color: 'green' }}>Cadastrado com Sucesso</p>}
             <InputField
                 label={"Nome: "}
                 type={"text"}
